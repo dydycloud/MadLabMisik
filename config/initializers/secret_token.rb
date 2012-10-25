703c6100400a165d8597296e544a6e37a37ae1a9
dydycloud <sandy.pierre97@gmail.com>
2012-10-25 17:01:23 +0200

    base of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madlab::Application.config.secret_token = 'a7adeefdfbd75579786eea14c27e521dfd8a8bb701e4972c44ab8320ce3c752c776ad9b125cae58e32781ad83f1658421715e99e0bdf2908f01649b7efff4154'
