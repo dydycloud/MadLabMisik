50bbdba3ab1a01243c932b884194921900ae776b
dydycloud <sandy.pierre97@gmail.com>
2012-10-30 15:43:29 +0100

    add auth and mail

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
-  before_filter :authenticate_user!
   attr_accessible :content, :description, :name, :title, :image_file_name
   attr_accessible :image
   has_attached_file :image
