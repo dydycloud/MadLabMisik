a2bce21cd11272108506f8e9aeb3c3d4eb9ee467
dydycloud <sandy.pierre97@gmail.com>
2012-10-30 15:35:13 +0100

    add auth and mail

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :description, :name, :title, :image_file_name
   attr_accessible :image
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Madlab::Application.routes.draw do
 
+  devise_for :users do
+	get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   get "static_pages/about"
 
   get "static_pages/contact"
